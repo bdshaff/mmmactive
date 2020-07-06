20a3921a37106e815b1a261fb3215cf5ef5dba2c
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-07-06 15:28:59 -0400

    docu
    docu

---
+++ R/mmmactive-package.R
@@ -0,0 +1,10 @@
+#' mmmactive: A package for computating the notorious bar statistic.
+#'
+#' mmmactive package: browseVignettes(package = "mmmactive")
+#'
+#' @section Core Idea:
+#' some desc
+#'
+#' @docType package
+#' @name mmmactive
+NULL
