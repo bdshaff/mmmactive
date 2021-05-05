1aeed426441c95faf5f6a205ec8ea0f3a609e143
bdshaff <52110434+bdshaff@users.noreply.github.com>
2021-05-05 17:58:00 +0300

    update activate_dummy_spec
    ability to activate from data.frame or path to .csv file

--- R/activate_dummy_spec.R
+++ R/activate_dummy_spec.R
@@ -11,8 +11,13 @@
 #' @importFrom magrittr %>%
 #' @export
 
-activate_dummy_spec = function(mod_obj, input_file_DummySpec){
-  dummy_spec = read_csv(input_file_DummySpec)
-  mod_obj$dummy_spec = dummy_spec
+activate_dummy_spec = function(mod_obj, input_file_DummySpec = NULL, dummy_spec_table = NULL){
+
+  if(!is.null(input_file_DummySpec)){
+    dummy_spec = read_csv(input_file_DummySpec)
+    mod_obj$dummy_spec = dummy_spec
+  }else if(!is.null(dummy_spec_table)){
+    mod_obj$dummy_spec = dummy_spec_table
+  }
   return(mod_obj)
 }
