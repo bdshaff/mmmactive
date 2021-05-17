7826b6e992c7689c7acc03fa1a77d3af1ddb4798
bdshaff <52110434+bdshaff@users.noreply.github.com>
2021-05-17 23:40:55 +0300

    Update activate_dummy_spec.R
    hey hey

--- R/activate_dummy_spec.R
+++ R/activate_dummy_spec.R
@@ -12,7 +12,7 @@
 #' @export
 
 activate_dummy_spec = function(mod_obj, input_file_DummySpec = NULL, dummy_spec_table = NULL){
-
+#hey hey
   if(!is.null(input_file_DummySpec)){
     dummy_spec = read_csv(input_file_DummySpec)
     mod_obj$dummy_spec = dummy_spec
